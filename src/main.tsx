573f39cb02763b1c2e84ae3d56f7d83cab3419bc
BrunoL326 <bruno.324156@gmail.com>
2023-08-16 19:31:01 -0300

    Maneger for routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './Start/App'
+import Changes from './Sobre/Mim';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 .render(
   <React.StrictMode>
     <App />
+    <Changes/>
   </React.StrictMode>,
 );
 
