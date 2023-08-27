fb745b4bcbf23cc92b97abb935258f6eb171dceb
BrunoL326 <bruno.324156@gmail.com>
2023-08-27 13:43:34 -0300

    Update-2

--- src/Starter/Starter.tsx
+++ src/Starter/Starter.tsx
@@ -1,5 +1,8 @@
 import Perfil from './EU2.jpg'
 import { BiUserCircle }  from '@react-icons/all-files/bi/BiUserCircle'
+import { AiOutlineInstagram } from '@react-icons/all-files/ai/AiOutlineInstagram'
+import { AiFillLinkedin } from '@react-icons/all-files/ai/AiFillLinkedin'
+import { AiFillGithub } from '@react-icons/all-files/ai/AiFillGithub'
 import  { Link } from 'react-router-dom';
 import './Starter.css'
 
@@ -9,14 +12,36 @@ function Starter() {
             <div className='perf-1'>
                 <img src={Perfil} className='Eu-2'/>
             </div>
+            <h1>
+                Sejam Bem Vindos ao Meu Mundo
+            </h1>
             <div className='Content-1'>
             <Link to='/Home'>
                 <a className='Container-1'>
                 <BiUserCircle/>
              </a>
              </Link>
+                </div> 
+             <div className='Content-2'>
+                 <a href="https://www.linkedin.com/in/bruno-lima-871a98252/" 
+                  className='Container-2'>
+                 <AiFillLinkedin/>
+             </a>
              </div>
-        </div>
+             <div className='Content-3'>
+                 <a href='https://www.instagram.com/b.l.o_buuh/' 
+                 className='Container-3'>
+                 <AiOutlineInstagram />
+             </a>
+             </div>
+             <div className='Content-4'>
+                 <a href='https://github.com/BrunoL326' 
+                 className='Container-4'>
+                 <AiFillGithub />
+             </a>
+             </div>
+             
+            </div>
     )
 }
 
--- src/componentes/Card/Cards.tsx
+++ src/componentes/Card/Cards.tsx
@@ -1,18 +1,18 @@
 import  { Link } from 'react-router-dom';
 import './Card.css';
 
-
 export function Cards(){
-
     return (
         <div className="costas">
     
             <div className='container'>
                 <a className='botão b_3'>Por Que</a>
+                
             </div>
               
             <div className='container'>
                 <a className='botão b_2'>Como</a>
+
             </div>
             
             <div className='container'>
