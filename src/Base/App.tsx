fe8bb331dd2ad17a69c591ee18ddbce5cc15b619
BrunoL326 <bruno.324156@gmail.com>
2024-04-15 21:50:50 -0300

    Atualização 35

--- src/Base/App.tsx
+++ src/Base/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
-import { Page1 } from '../Pages/Como/como';
 import { Page2 } from '../Pages/Sobre/Mim';
 import { Page3 } from '../Pages/Porque/why';
 import './styles.css';
@@ -12,7 +11,6 @@ import Starter from '../Pages/Starter/Starter';
         <Routes>
           <Route path='/' element={<Starter />}/>
           <Route path="/Home" element={<Home />}/>
-          <Route path="/Como" element={<Page1 />}/>
           <Route path="/Sobre" element={<Page2 />}/>
           <Route path="/Porque" element={<Page3 />}/>
         </Routes>
--- src/componentes_base/Card/Cards.tsx
+++ src/componentes_base/Card/Cards.tsx
@@ -1,27 +1,40 @@
+import { useState } from 'react'
 import  { Link } from 'react-router-dom';
 import './Card.css';
 
 export function Cards(){
+
+    const [ Active, DESLIGADO ] = useState(false)
+
     return (
         <div className="costas">
     
             <div className='container'>
-                <Link to='/Porque' className='botão b_3'>Porque</Link>
+                <Link to='/Porque' className='botão b_3'>PORQUE</Link>
                 
             </div>
               
             <div className='container'>
-            <Link to='/Sobre' className='botão b_2'>Sobre</Link>
+            <Link to='/Sobre' className='botão b_2'>SOBRE</Link>
 
             </div>
             
             <div className='container'>
-                <Link to='/Como' className='botão b_1'>Como</Link>
+            <div onClick={() => {DESLIGADO(!Active)}}>
+                <div className='botão b_1'>COMO</div>
             </div>
-            
-         </div>
-            
-    )
+        {Active && (       
+        <div className="body_5">
+            <div className="card_0">
+                <div className="content_5" >
+                    <p>Ao entender que, no futuro, eu seria um adulto, tomei uma decisão há muito tempo sobre quem eu seria e como seria. Eu queria mais do que tudo ter paz e minhas aventuras, então me dediquei totalmente a ser um funcionário exemplar onde quer que eu trabalhasse. Desde cedo, aprendi a mexer no computador de uma forma diferente de um usuário comum. Me matriculei em diversos cursos paralelos de administração e informática. Até que um amigo me deu a melhor indicação que já recebi. Além de me ensinar muito do que sabia, através do site da Rocketseat, obtive muitos resultados. Hoje em dia, já consigo criar desde sites até aplicativos completos e continuo me esforçando para me aperfeiçoar e alcançar cargos melhores.</p>
+                    </div>
+            </div>
+        </div>
+)};
+        </div>
+        </div>
+    );
 }
-
-export default Cards
+        
+    
